129d8856f7af1ca63704d0e476e2d4d7fec66b0f
KatherineEvans <Evans.Katherine.E@gmail.com>
2018-07-01 10:56:29 -0500

    cleaned up code

--- app/models/my_contact.rb
+++ app/models/my_contact.rb
@@ -1,15 +1,15 @@
 class MyContact < ApplicationRecord
 
   def friendly_updated_at
-    created_at.strftime("%B %e, %Y")
+    updated_at.strftime("%B %e, %Y")
   end
 
   def full_name
-    full_name = "#{first_name} #{last_name}"
+    "#{first_name} #{last_name}"
   end
 
   def japan_phone
-    japan_phone = "+81 #{phone_number}"
+    "+81 #{phone_number}"
   end
 
 end
